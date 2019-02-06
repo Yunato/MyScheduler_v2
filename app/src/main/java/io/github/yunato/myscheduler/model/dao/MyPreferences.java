f9af21003c094d4e287ef813ec19d8e669d64efe
Yunato <yunato525@gmail.com>
2019-02-06 20:14:59 +0900

    Refactoring at MyPreferences

--- app/src/main/java/io/github/yunato/myscheduler/model/dao/MyPreferences.java
+++ app/src/main/java/io/github/yunato/myscheduler/model/dao/MyPreferences.java
@@ -8,23 +8,22 @@ import static android.content.Context.MODE_PRIVATE;
 public class MyPreferences {
     private final SharedPreferences preferences;
 
-    /** 識別子 **/
+    /** 識別子 */
     public static final String IDENTIFIER_PREF = "MY_PREFERENCE";
     public static final String PREF_ACCOUNT_NAME = "accountName";
     static final String IDENTIFIER_LOCAL_ID = "LOCAL_CALENDAR_ID";
     static final String IDENTIFIER_REMOTE_ID = "REMOTE_CALENDAR_ID";
 
-    MyPreferences(Context context){
-        preferences = context.getSharedPreferences(IDENTIFIER_PREF , MODE_PRIVATE);
+    MyPreferences(Context context) {
+        preferences = context.getSharedPreferences(IDENTIFIER_PREF, MODE_PRIVATE);
     }
 
-    String getValue(String key){
+    String getValue(String key) {
         return preferences.getString(key, null);
     }
 
-    void setValue(String key, String value){
+    void setValue(String key, String defValue) {
         SharedPreferences.Editor e = preferences.edit();
-        e.putString(key, value);
-        e.apply();
+        e.putString(key, defValue).apply();
     }
 }
