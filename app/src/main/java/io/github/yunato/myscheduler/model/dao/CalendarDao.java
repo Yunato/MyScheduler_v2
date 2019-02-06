617aad3d70a85385df8acfa232f5a2ac2d2c99b6
Yunato <yunato525@gmail.com>
2019-02-06 20:11:17 +0900

    Refactoring at CalendarDao

--- app/src/main/java/io/github/yunato/myscheduler/model/dao/CalendarDao.java
+++ app/src/main/java/io/github/yunato/myscheduler/model/dao/CalendarDao.java
@@ -6,26 +6,8 @@ abstract class CalendarDao {
     private final Context context;
     final MyPreferences myPreferences;
 
-    CalendarDao(Context context){
+    CalendarDao(Context context) {
         this.context = context;
         this.myPreferences = new MyPreferences(context);
     }
-
-    /**
-     * プリファレンスから値を取得する
-     * @param key       キー
-     * @return          値
-     *//*
-    public String getValueFromPref(String key){
-        return myPreferences.getValue(key);
-    }*/
-
-    /**
-     * プリファレンスへ値を設定する
-     * @param key       キー
-     * @param value     値
-     *//*
-    public void setValueToPref(String key, String value){
-        myPreferences.setValue(key, value);
-    }*/
 }
