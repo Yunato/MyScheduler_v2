9cfb2c7542efdfb1b309e79e1f36cd7e782e58af
Yunato <yunato525@gmail.com>
2019-02-06 20:17:55 +0900

    Refactoring at EventDao

--- app/src/main/java/io/github/yunato/myscheduler/model/dao/EventDao.java
+++ app/src/main/java/io/github/yunato/myscheduler/model/dao/EventDao.java
@@ -10,27 +10,9 @@ abstract class EventDao {
     private final Context context;
     private final MyPreferences myPreferences;
 
-    EventDao(Context context){
+    EventDao(Context context) {
         this.context = context;
         this.myPreferences = new MyPreferences(context);
-    }
-
-    /**
-     * プリファレンスから値を取得する
-     * @param key       キー
-     * @return          値
-     */
-    public String getValueFromPref(String key){
-        return myPreferences.getValue(key);
-    }
-
-    /**
-     * プリファレンスへ値を設定する
-     * @param key       キー
-     * @param value     値
-     */
-    public void setValueToPref(String key, String value){
-        myPreferences.setValue(key, value);
     }
 
     public abstract List<String> insertEventItems(List<EventInfo.EventItem> eventItems);
