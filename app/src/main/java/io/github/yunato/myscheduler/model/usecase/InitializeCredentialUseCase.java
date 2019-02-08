839a9e7aacba64d3d6d5949542fd9d11e4759764
Yunato <yunato525@gmail.com>
2019-02-08 22:24:02 +0900

    Add InitializeCredentialUseCase

---
+++ app/src/main/java/io/github/yunato/myscheduler/model/usecase/InitializeCredentialUseCase.java
@@ -0,0 +1,31 @@
+package io.github.yunato.myscheduler.model.usecase;
+
+
+import android.content.Context;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.calendar.CalendarScopes;
+
+import java.util.Arrays;
+
+/**
+ * 認証情報を初期化するユースケース
+ */
+public class InitializeCredentialUseCase {
+
+    private final Context context;
+    private static final String[] SCOPES = {CalendarScopes.CALENDAR};
+
+    public InitializeCredentialUseCase(Context context) {
+        this.context = context;
+    }
+
+    public GoogleAccountCredential run() {
+        return GoogleAccountCredential.usingOAuth2(
+                context.getApplicationContext(),
+                Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
+    }
+
+}
