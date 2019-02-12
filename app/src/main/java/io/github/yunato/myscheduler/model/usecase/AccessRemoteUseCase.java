544e3bca1aacd8697460761c157fa0db7dccc3ec
Yunato <yunato525@gmail.com>
2019-02-13 00:18:15 +0900

    Refactor AccessRemoteUseCase [application can't run]

--- app/src/main/java/io/github/yunato/myscheduler/model/usecase/AccessRemoteUseCase.java
+++ app/src/main/java/io/github/yunato/myscheduler/model/usecase/AccessRemoteUseCase.java
@@ -1,16 +1,16 @@
 package io.github.yunato.myscheduler.model.usecase;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-
-import java.util.List;
-
-import io.github.yunato.myscheduler.model.entity.EventItem;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
 
 public abstract class AccessRemoteUseCase {
-    private final Activity activity;
+    protected final Activity activity;
 
     /** 要求コード */
     public static final int REQUEST_ACCOUNT_PICKER = 1000;
@@ -18,20 +18,20 @@ public abstract class AccessRemoteUseCase {
     public static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
     public static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
 
-    public AccessRemoteUseCase(Activity activity) {
+    // int nextState, List<EventItem> eventItems 必要なクラスはコンストラクタで受け取る
+    AccessRemoteUseCase(Activity activity) {
         this.activity = activity;
     }
 
-    public abstract void run();
-
-    public void callGoogleApi(int nextState, List<EventItem> eventItems) {
+    public void run() {
         if (!isGooglePlayServicesAvailable()) {
             acquireGooglePlayServices();
         } else {
-            run();
+            callGoogleApi();
         }
-
     }
+
+    protected abstract void callGoogleApi();
 
     private boolean isGooglePlayServicesAvailable() {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
@@ -43,11 +43,40 @@ public abstract class AccessRemoteUseCase {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
         final int connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(activity);
         if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
-            apiAvailability.getErrorDialog(
-                    activity,
-                    connectionStatusCode,
-                    REQUEST_GOOGLE_PLAY_SERVICES
-            ).show();
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+    private void showGooglePlayServicesAvailabilityErrorDialog(int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        apiAvailability.getErrorDialog(
+                activity,
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES
+        ).show();
+    }
+
+    private void showUserRecoverableAuthDialog(Intent intent) {
+        activity.startActivityForResult(intent, REQUEST_AUTHORIZATION);
+    }
+
+    abstract class RequestTask extends AsyncTask<Void, Void, Void>{
+        Exception mLastError = null;
+
+        @Override
+        protected void onCancelled() {
+            if (mLastError != null) {
+                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                    GooglePlayServicesAvailabilityIOException exception =
+                            (GooglePlayServicesAvailabilityIOException) mLastError;
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                            exception.getConnectionStatusCode());
+                } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                    UserRecoverableAuthIOException exception =
+                            (UserRecoverableAuthIOException) mLastError;
+                    showUserRecoverableAuthDialog(exception.getIntent());
+                }
+            }
         }
     }
 }
