98a2a64dc3fe670b9ed4313ba23cf8923c5d5360
Yunato <yunato525@gmail.com>
2019-02-14 00:16:10 +0900

    Refactor EventItemRepository

--- app/src/main/java/io/github/yunato/myscheduler/model/repository/EventItemRepository.java
+++ app/src/main/java/io/github/yunato/myscheduler/model/repository/EventItemRepository.java
@@ -2,28 +2,40 @@ package io.github.yunato.myscheduler.model.repository;
 
 import android.support.annotation.Nullable;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import io.github.yunato.myscheduler.model.dao.DaoFactory;
+import io.github.yunato.myscheduler.model.dao.LocalDao;
 import io.github.yunato.myscheduler.model.entity.EventItem;
-import io.github.yunato.myscheduler.model.usecase.GenerateEventUsecase;
+import io.github.yunato.myscheduler.model.usecase.GenerateEventUseCase;
 
 public class EventItemRepository {
-    private Calendar calendar = Calendar.getInstance();
-    public static List<EventItem> ITEMS = new ArrayList<>();
+
+    private static List<EventItem> ITEMS = new ArrayList<>();
+    private static Calendar calendar = Calendar.getInstance();
     //private static final Map<String, PlanItem> ITEM_MAP = new HashMap<>();
 
-    public Calendar getCalendar() {
-        return calendar;
+    public static void setEventItems(int year, int month, int dayOfMonth){
+        LocalDao localDao = DaoFactory.getLocalDao();
+        ITEMS = localDao.getEventItemsOnDay(year, month, dayOfMonth);
+        calendar.set(year, month, dayOfMonth);
     }
 
-    public void setCalendar(Calendar calendar) {
-        this.calendar = calendar;
+    public static List<EventItem> getEventItems(){
+        return ITEMS;
+    }
+
+    public static long getTimeInMillis(){
+        return calendar.getTimeInMillis();
     }
 
     public void createList(Calendar calendar) {
-        GenerateEventUsecase generator = new GenerateEventUsecase(calendar);
+        GenerateEventUseCase generator = new GenerateEventUseCase(calendar);
         ITEMS = generator.generate();
     }
 
@@ -40,13 +52,25 @@ public class EventItemRepository {
             String description,
             long startMillis,
             long endMillis) {
-        //TODO: 不適切なら null を返す (このメソッドを呼び出すusecaseが担うべきか)
         return new EventItem(eventId, title, description, startMillis, endMillis);
     }
 
     public static void add(EventItem item) {
-        //TODO: 日付が本日のものであれば追加を行えるようにEventItem のフィールドを比較する
         ITEMS.add(item);
         //ITEM_MAP.put(item.calendarId, item);
+    }
+
+    public static String convertDateToString(long time) {
+        Date date = new Date();
+        date.setTime(time);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日", Locale.JAPANESE);
+        return sdf.format(date);
+    }
+
+    public static String convertTimeToString(long time) {
+        Date date = new Date();
+        date.setTime(time);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.JAPANESE);
+        return sdf.format(date);
     }
 }
